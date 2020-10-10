722326d7bbdce2318f085710cf1fc25873b5b3d4
Jakob Persons <jakobep24@gmail.com>
2020-10-10 16:33:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
 end
 
 display_board(board)
